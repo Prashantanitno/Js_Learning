4561bd66d6f01a8b8612acaad82439ae1759570e
Prince016 <prashantsharma3854@gmail.com>
2023-07-31 22:52:29 +0530

    till hoisting question

--- hositing_scope_callBack_arrow_function.js
+++ hositing_scope_callBack_arrow_function.js
@@ -24,10 +24,48 @@ function displaySquare(fn) {
 
 (function (x) {
   return (function (y) {
-    console.log("x", x);
-    console.log("y", y);
+    // console.log("x", x);
+    // console.log("y", y);
   })(2);
 })(1);
 
 
 // first search the x in the return scope and then search for the x in the parent scope i.e whole funciton 
+
+
+
+
+
+
+// --------------->  What is Function Scope <------------------
+
+
+//----->   question on function scope <-----------
+
+// as let creats a block scope for this 
+
+// -----> For Var <------- 
+// for(var i=0; i<5; i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, i*1000);
+// }
+
+
+
+
+// -->  function Hoisting 
+
+
+// ---->  Question on hoisting <-------
+
+var x =21; 
+var fun = function(){
+    console.log("x-> ",x)
+    // var x=20;
+}
+fun();
+
+// Ans: undefinded because ->  first it is intialized the global scope then 
+// go to the local scope as  x has the value of 21 if inside var is not pressent  
+// because of the lcoal scope it is not been intailized so teh value is undefined
