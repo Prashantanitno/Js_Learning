ad12bd60e1f09a23c620614d0bde12eb070cab24
Prashantanitno <aditya555antino@gmail.com>
2023-07-31 21:23:33 +0530

    updated code of hoisting

--- hositing_scope_callBack_arrow_function.js
+++ hositing_scope_callBack_arrow_function.js
@@ -1,17 +1,33 @@
 // function in Javascript
 
-//  what are first class functions? 
+//  what are first class functions?
 
-//  when a function threated like a variable and can pass to another function this can do what a variable can do 
-function square(num){
-    return num*num;
+//  when a function threated like a variable and can pass to another function this can do what a variable can do
+function square(num) {
+  return num * num;
 }
 
-function displaySquare(fn){
-    console.log("square is "+fn(5));
+function displaySquare(fn) {
+  console.log("square is " + fn(5));
 }
 
-displaySquare(square);
+// displaySquare(square);
 
 // ----------> What is IIFE  <---------------
-// Immediate invoke function expressions 
+// Immediate invoke function expressions
+
+(function square(num) {
+  //   console.log(num * num);
+})(5);
+
+// Question on IIFE
+
+(function (x) {
+  return (function (y) {
+    console.log("x", x);
+    console.log("y", y);
+  })(2);
+})(1);
+
+
+// first search the x in the return scope and then search for the x in the parent scope i.e whole funciton 
