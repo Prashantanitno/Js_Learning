c3abf24a4c0813b109d6910aced50fd241a18191
Prince016 <prashantsharma3854@gmail.com>
2023-08-04 22:24:15 +0530

    till hoisting video complete

--- hositing_scope_callBack_arrow_function.js
+++ hositing_scope_callBack_arrow_function.js
@@ -29,43 +29,55 @@ function displaySquare(fn) {
   })(2);
 })(1);
 
-
-// first search the x in the return scope and then search for the x in the parent scope i.e whole funciton 
-
-
-
-
-
+// first search the x in the return scope and then search for the x in the parent scope i.e whole funciton
 
 // --------------->  What is Function Scope <------------------
 
-
 //----->   question on function scope <-----------
 
-// as let creats a block scope for this 
+// as let creats a block scope for this
 
-// -----> For Var <------- 
+// -----> For Var <-------
 // for(var i=0; i<5; i++){
 //     setTimeout(() => {
 //         console.log(i)
 //     }, i*1000);
 // }
 
-
-
-
-// -->  function Hoisting 
-
+// -->  function Hoisting
 
 // ---->  Question on hoisting <-------
 
-var x =21; 
-var fun = function(){
-    console.log("x-> ",x)
-    // var x=20;
-}
-fun();
+var x = 21;
+var fun = function () {
+  console.log("x-> ", x);
+  // var x=20;
+};
+// fun();
 
-// Ans: undefinded because ->  first it is intialized the global scope then 
-// go to the local scope as  x has the value of 21 if inside var is not pressent  
-// because of the lcoal scope it is not been intailized so teh value is undefined
+// Ans: undefinded because ->  first it is intialized the global scope then
+// go to the local scope as  x has the value of 21 if inside var is not pressent bbecause of the lcoal scope it is not been intailized so teh value is undefined
+
+// ----------> Spread Vs Rest Operators <--------------
+
+// function multiple (num,num2){
+function multiple(...nums) {
+  // ...nums => this is called rest operators
+  console.log(nums[0] * nums[1]);
+}
+
+var arr = [5, 6];
+// multiple(...arr); // ...arr => this is called spread operator
+// this is a spread operator when we need to give both of these values to this funciton
+
+// Questions
+
+const fn = (a, x, y, ...nums) => {
+  console.log(x, y,nums);
+};
+
+fn(3, 4, 5, 6, 6, 7, 8, 8);
+
+
+// Call Back functions 
+
